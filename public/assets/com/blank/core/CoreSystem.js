29e2f721ba8f3f93414998f98ea1ed4ed1916480
blank <heroblank@gmail.com>
2016-09-27 11:20:33 +0800

    优化用户数据读取部分.

--- public/assets/com/blank/core/CoreSystem.js
+++ public/assets/com/blank/core/CoreSystem.js
@@ -5,14 +5,14 @@ CloudOS.CoreSystem = (function(){
 	var CoreSystem = {};
 	CoreSystem.start = function()
 	{
-		CloudOS.User.login("guest","123456",function(){
-	    	CloudOS.User.currentUser.read(function(){
+		CloudOS.User.login("guest","123456",function(err){
+			if(err) return trace(err);
+	    	CloudOS.User.currentUser.read(function(err){
+	    		if(err) return trace(err);
 		    	CSSPlugin.defaultTransformPerspective = 1000;
 				CoreSystem.desktop = new CloudOS.Desktop();
 				CoreSystem.desktop.view.appendTo('body');
 	    	});
-	    },function(err){
-	    	trace(err);
 	    });
 	}
 	return CoreSystem;
--- public/assets/com/blank/net/User.js
+++ public/assets/com/blank/net/User.js
@@ -12,35 +12,23 @@ CloudOS.User = (function(){
 	
 	User.prototype.read = function(_onComplete)
 	{
-	    var self = this;
-	    $.getJSON(CloudOS.Request.User.config,{name:this.name,token:this.token},function(_data){
-	        if(_data.status == "success")
-	        {
-	        	self.config = _data.result;
-	        	_onComplete();
-	        }
-	        else
-	        {
-	        	trace(_data);
-	        }
+		var self = this;
+	    return new CloudOS.Loader(CloudOS.Request.User.config,{name:this.name,token:this.token},function(err,_data){
+	    	if(err) return _onComplete(err);
+	    	self.config = _data;
+	    	_onComplete();
 	    });
 	}
 	
 	User.currentUser = null;
 	
-	User.login = function(_name,_password,_onSuccess,_onFail)
+	User.login = function(_name,_password,_onComplete)
 	{
-	    $.getJSON(CloudOS.Request.login,{name:_name,password:_password},function(_data){
-	    	if(_data.status == "success")
-	        {
-	            User.currentUser = new User(_name,_data.result.token);
-	            _onSuccess();
-	        }
-	        else
-	        {
-	        	_onFail(_data);
-	        }
-	    });
+		return new CloudOS.Loader(CloudOS.Request.login,{name:_name,password:_password},function(err,_data){
+	   		if(err) return _onComplete(err);
+	   		User.currentUser = new User(_name,_data.token);
+	   		_onComplete();
+	   	});
 	}
 	
 	return User;
